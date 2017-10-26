7bfc6a6b38d7e063adc099db7d040b5dc74d73f4
yumochi <yumochi2@illinois.edu>
2017-10-25 23:19:48 -0500

    new file:   jsonDeserializer/.vs/jsonDeserializer/v15/.suo 	new file:   jsonDeserializer/.vs/jsonDeserializer/v15/sqlite3/storage.ide 	new file:   jsonDeserializer/jsonDeserializer.sln 	new file:   jsonDeserializer/jsonDeserializer/App.config 	new file:   jsonDeserializer/jsonDeserializer/Program.cs 	new file:   jsonDeserializer/jsonDeserializer/Properties/AssemblyInfo.cs 	new file:   jsonDeserializer/jsonDeserializer/bin/Debug/Newtonsoft.Json.dll 	new file:   jsonDeserializer/jsonDeserializer/bin/Debug/Newtonsoft.Json.xml 	new file:   jsonDeserializer/jsonDeserializer/bin/Debug/jsonDeserializer.exe 	new file:   jsonDeserializer/jsonDeserializer/bin/Debug/jsonDeserializer.exe.config 	new file:   jsonDeserializer/jsonDeserializer/bin/Debug/jsonDeserializer.pdb 	new file:   jsonDeserializer/jsonDeserializer/jsonDeserializer.csproj 	new file:   jsonDeserializer/jsonDeserializer/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache 	new file:   jsonDeserializer/jsonDeserializer/obj/Debug/TemporaryGeneratedFile_036C0B5B-1481-4323-8D20-8F5ADCB23D92.cs 	new file:   jsonDeserializer/jsonDeserializer/obj/Debug/TemporaryGeneratedFile_5937a670-0e60-4077-877b-f7221da3dda1.cs 	new file:   jsonDeserializer/jsonDeserializer/obj/Debug/TemporaryGeneratedFile_E7A71F73-0F8D-4B9B-B56E-8E70B10BC5D3.cs 	new file:   jsonDeserializer/jsonDeserializer/obj/Debug/jsonDeserializer.csproj.CopyComplete 	new file:   jsonDeserializer/jsonDeserializer/obj/Debug/jsonDeserializer.csproj.CoreCompileInputs.cache 	new file:   jsonDeserializer/jsonDeserializer/obj/Debug/jsonDeserializer.csproj.FileListAbsolute.txt 	new file:   jsonDeserializer/jsonDeserializer/obj/Debug/jsonDeserializer.csprojResolveAssemblyReference.cache 	new file:   jsonDeserializer/jsonDeserializer/obj/Debug/jsonDeserializer.exe 	new file:   jsonDeserializer/jsonDeserializer/obj/Debug/jsonDeserializer.pdb 	new file:   jsonDeserializer/jsonDeserializer/packages.config 	new file:   jsonDeserializer/packages/Newtonsoft.Json.10.0.3/LICENSE.md 	new file:   jsonDeserializer/packages/Newtonsoft.Json.10.0.3/Newtonsoft.Json.10.0.3.nupkg 	new file:   jsonDeserializer/packages/Newtonsoft.Json.10.0.3/lib/net20/Newtonsoft.Json.dll 	new file:   jsonDeserializer/packages/Newtonsoft.Json.10.0.3/lib/net20/Newtonsoft.Json.xml 	new file:   jsonDeserializer/packages/Newtonsoft.Json.10.0.3/lib/net35/Newtonsoft.Json.dll 	new file:   jsonDeserializer/packages/Newtonsoft.Json.10.0.3/lib/net35/Newtonsoft.Json.xml 	new file:   jsonDeserializer/packages/Newtonsoft.Json.10.0.3/lib/net40/Newtonsoft.Json.dll 	new file:   jsonDeserializer/packages/Newtonsoft.Json.10.0.3/lib/net40/Newtonsoft.Json.xml 	new file:   jsonDeserializer/packages/Newtonsoft.Json.10.0.3/lib/net45/Newtonsoft.Json.dll 	new file:   jsonDeserializer/packages/Newtonsoft.Json.10.0.3/lib/net45/Newtonsoft.Json.xml 	new file:   jsonDeserializer/packages/Newtonsoft.Json.10.0.3/lib/netstandard1.0/Newtonsoft.Json.dll 	new file:   jsonDeserializer/packages/Newtonsoft.Json.10.0.3/lib/netstandard1.0/Newtonsoft.Json.xml 	new file:   jsonDeserializer/packages/Newtonsoft.Json.10.0.3/lib/netstandard1.3/Newtonsoft.Json.dll 	new file:   jsonDeserializer/packages/Newtonsoft.Json.10.0.3/lib/netstandard1.3/Newtonsoft.Json.xml 	new file:   jsonDeserializer/packages/Newtonsoft.Json.10.0.3/lib/portable-net40+sl5+win8+wp8+wpa81/Newtonsoft.Json.dll 	new file:   jsonDeserializer/packages/Newtonsoft.Json.10.0.3/lib/portable-net40+sl5+win8+wp8+wpa81/Newtonsoft.Json.xml 	new file:   jsonDeserializer/packages/Newtonsoft.Json.10.0.3/lib/portable-net45+win8+wp8+wpa81/Newtonsoft.Json.dll 	new file:   jsonDeserializer/packages/Newtonsoft.Json.10.0.3/lib/portable-net45+win8+wp8+wpa81/Newtonsoft.Json.xml 	new file:   jsonDeserializer/packages/Newtonsoft.Json.10.0.3/tools/install.ps1

---
+++ jsonDeserializer/jsonDeserializer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace jsonDeserializer
+{
+    public class Account
+    {
+        public string Email { get; set; }
+        public bool Active { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public IList<string> Roles { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string json = @"{
+                'Email': 'james@example.com',
+                'Active': true,
+                'CreatedDate': '2013-01-20T00:00:00Z',
+                'Roles': [
+                    'User',
+                    'Admin'
+                ]
+            }";
+
+            Account account = JsonConvert.DeserializeObject<Account>(json);
+            Console.WriteLine(account.Email);
+        }
+    }
+}
