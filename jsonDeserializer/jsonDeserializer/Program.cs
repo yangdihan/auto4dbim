40d81a7a719b401ba71bd97d1cc2f702fdce3fa5
yumochi <yumochi2@illinois.edu>
2017-11-01 17:54:35 -0500

    modified:   jsonDeserializer/.vs/jsonDeserializer/v15/.suo 	modified:   jsonDeserializer/.vs/jsonDeserializer/v15/sqlite3/storage.ide 	modified:   jsonDeserializer/jsonDeserializer/Program.cs 	modified:   jsonDeserializer/jsonDeserializer/api_input_test.json 	modified:   jsonDeserializer/jsonDeserializer/bin/Debug/jsonDeserializer.exe 	modified:   jsonDeserializer/jsonDeserializer/bin/Debug/jsonDeserializer.pdb 	modified:   jsonDeserializer/jsonDeserializer/obj/Debug/jsonDeserializer.exe 	modified:   jsonDeserializer/jsonDeserializer/obj/Debug/jsonDeserializer.pdb 	jsonDeserializer/jsonDeserializer/parsed.json

--- jsonDeserializer/jsonDeserializer/Program.cs
+++ jsonDeserializer/jsonDeserializer/Program.cs
@@ -8,6 +8,11 @@ using Newtonsoft.Json;
 
 namespace jsonDeserializer
 {
+    public class levelAllocation
+    {
+        public Dictionary<string, zoneAllocation> Levels { set; get; }
+    }
+
     public class zoneAllocation
     {
         public Dictionary<string, zone> Zones { set; get; }
@@ -15,10 +20,10 @@ namespace jsonDeserializer
 
     public class zone
     {
-        public string top { get; set; }
         public string bottom { get; set; }
         public string left { get; set; }
         public string right { get; set; }
+        public string top { get; set; }
     }
 
     class Program
@@ -27,23 +32,27 @@ namespace jsonDeserializer
         {
             // import a file into code
             // change path to load different files
-            string path = @"C:\Users\raamac\auto4dbim\jsonDeserializer\jsonDeserializer\api_input_test.json";
+            string path = @"C:\Users\raamac\auto4dbim\jsonDeserializer\jsonDeserializer\parsed.json";
 
             try
             {   // Open the text file using a stream reader.
                 using (StreamReader file = File.OpenText(path))
                 {
                     Console.WriteLine("inside file");
-                    var res = JsonConvert.DeserializeObject<zoneAllocation>(File.ReadAllText(path));
+                    var res = JsonConvert.DeserializeObject<levelAllocation>(File.ReadAllText(path));
                     Console.WriteLine("res created");
-                    foreach (var f in res.Zones)
+                    foreach (var f in res.Levels)
                     {
                         Console.WriteLine("inside forloop");
-                        Console.WriteLine("zone: {0}",f.Key);
-                        Console.WriteLine("top: {0}", f.Value.top);
-                        Console.WriteLine("bottom: {0}", f.Value.bottom);
-                        Console.WriteLine("left: {0}", f.Value.left);
-                        Console.WriteLine("right: {0}", f.Value.right);
+                        Console.WriteLine("level: {0}", f.Key);
+                        foreach (var fi in f.Value.Zones)
+                        {
+                            Console.WriteLine("zone: {0}", fi.Key);
+                            Console.WriteLine("top: {0}", fi.Value.top);
+                            Console.WriteLine("bottom: {0}", fi.Value.bottom);
+                            Console.WriteLine("left: {0}", fi.Value.left);
+                            Console.WriteLine("right: {0}", fi.Value.right);
+                        }
                     }
 
                 }
@@ -53,7 +62,6 @@ namespace jsonDeserializer
                 Console.WriteLine("The file could not be read:");
                 Console.WriteLine(e.Message);
             }
-
 
         }
     }
