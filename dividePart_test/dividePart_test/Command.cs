169f04d51ef27e9f43d9f3ac83e9fa86f53f0e9e
yangdihan <dyang42@illinois.edu>
2017-11-01 18:55:51 -0500

    each level now can be divided by different zones

--- dividePart_test/dividePart_test/Command.cs
+++ dividePart_test/dividePart_test/Command.cs
@@ -15,19 +15,13 @@ using Newtonsoft.Json;
 using jsonDeserializer;
 #endregion
 
-namespace dividePart_test
-{
+namespace dividePart_test {
     [Transaction(TransactionMode.Manual)]
     // copied class from the Building Coder
 
-    public class Command : IExternalCommand
-    {
+    public class Command : IExternalCommand {
 
-        public Result Execute(
-          ExternalCommandData commandData,
-          ref string message,
-          ElementSet elements)
-        {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements) {
             UIApplication uiapp = commandData.Application;
             UIDocument uidoc = uiapp.ActiveUIDocument;
             Application app = uiapp.Application;
@@ -35,9 +29,10 @@ namespace dividePart_test
             View view = doc.ActiveView;
             //TaskDialog.Show("Revit", "Hello World");
 
-        // reading JSON file:
-            string path = @"D:\Documents\Revit Model\api_input_test.json";
-            var res = JsonConvert.DeserializeObject<zoneAllocation>(File.ReadAllText(path));
+
+            // reading JSON file:
+            string path = @"D:\Documents\Revit Model\parsed.json";
+            var res = JsonConvert.DeserializeObject<levelAllocation>(File.ReadAllText(path));
 
             // Create filters and collect walls/ slabs/ columns id in corresponding collector
             // walls
@@ -48,16 +43,15 @@ namespace dividePart_test
                     walls_id.Add(w.Id);
                 }
             }
-        // columns
+            // columns
             IList<ElementId> columns_id = new List<ElementId>();
             FilteredElementCollector column_collector = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_StructuralColumns);
             foreach (Element c in column_collector) {
-                if (c is FamilyInstance)
-                {
+                if (c is FamilyInstance) {
                     columns_id.Add(c.Id);
                 }
             }
-        // slabs
+            // slabs
             IList<ElementId> slabs_id = new List<ElementId>();
             FilteredElementCollector collector = new FilteredElementCollector(doc).OfClass(typeof(Floor));
             foreach (Element e in collector) {
@@ -66,7 +60,7 @@ namespace dividePart_test
                 }
             }
 
-        // filter out all levels and grids
+            // filter out all levels and grids
             ICollection<ElementId> levels = new FilteredElementCollector(doc).OfClass(typeof(Level)).OfCategory(BuiltInCategory.OST_Levels).ToElementIds();
             ICollection<ElementId> grids = new FilteredElementCollector(doc).OfClass(typeof(Grid)).OfCategory(BuiltInCategory.OST_Grids).ToElementIds();
 
@@ -82,10 +76,9 @@ namespace dividePart_test
                 t.Commit();
             }
 
-    // start divide parts for walls, columns, slabs
-        // divide walls
-            foreach (ElementId w_id in walls_id)
-            {
+            // start divide parts for walls, columns, slabs
+            // divide walls
+            foreach (ElementId w_id in walls_id) {
                 ICollection<ElementId> partsList = PartUtils.GetAssociatedParts(doc, w_id, true, true);
 
                 // Create a list of curves which needs to be used in DivideParts but for this example
@@ -100,8 +93,7 @@ namespace dividePart_test
                 // Will be used to create a sketch plane
                 Reference r = HostObjectUtils.GetSideFaces(hostObj, ShellLayerType.Exterior).First();
 
-                using (Transaction t = new Transaction(doc, "Divide Part at Levels"))
-                {
+                using (Transaction t = new Transaction(doc, "Divide Part at Levels")) {
                     t.Start();
                     //Plane ref_plane = Plane.CreateByNormalAndOrigin(faceNormal, XYZ.Zero);
                     SketchPlane wall_sketchPlane = SketchPlane.Create(doc, r);
@@ -112,7 +104,7 @@ namespace dividePart_test
                 }
             }
 
-        // divide columns
+            // divide columns
             // since walls and columns are all divided by all levels, so just use the sketch-plane of the last wall element
             ElementId borrow_from_wall = walls_id[0];
             foreach (ElementId c_id in columns_id) {
@@ -135,93 +127,99 @@ namespace dividePart_test
 
             // divide slabs
             foreach (ElementId s_id in slabs_id) {
+                Floor this_slab = doc.GetElement(s_id) as Floor;
+                ElementId slab_level_id = this_slab.LevelId;
+                string slab_level = doc.GetElement(slab_level_id).Name;
+                foreach (var f in res.Levels) {
+                    if (f.Key == slab_level) {
+                        // ready to divide
+                        ICollection<ElementId> partsList = PartUtils.GetAssociatedParts(doc, s_id, true, true);
 
-                ICollection<ElementId> partsList = PartUtils.GetAssociatedParts(doc, s_id, true, true);
-                // find z of slab's top face
-                BoundingBoxXYZ bbox = doc.GetElement(s_id).get_BoundingBox(view);
-                double slab_top_face_z = bbox.Max.Z;
-                // all grids elementid are now in grids
-                IList<Curve> curve_trim = new List<Curve>();
-                // loop over all zones
-                foreach (var one_of_zone in res.Zones)
-                {
-                    // find four bounding curve for one zone, not sure if itersection is counted to trim curves
-                    IList<Curve> cur_bound_box = new List<Curve>();
+                        // find z of slab's top face
+                        BoundingBoxXYZ bbox = doc.GetElement(s_id).get_BoundingBox(view);
+                        double slab_top_face_z = bbox.Max.Z;
+                        // all grids elementid are now in grids
+                        IList<Curve> curve_trim = new List<Curve>();
 
-                    // not yet used, need for later "mark" feature
-                    string cur_zone_name = one_of_zone.Key;
+                        // loop over all zones
+                        foreach (var one_of_zone in f.Value.Zones) {
+                            // find four bounding curve for one zone, not sure if itersection is counted to trim curves
+                            IList<Curve> cur_bound_box = new List<Curve>();
 
-                    // loop over all grids
-                    foreach (ElementId grid_id in grids)
-                    {
-                        
-                        Grid cur = doc.GetElement(grid_id) as Grid;
-                        string cur_name = cur.Name;
-                        if (cur_name == one_of_zone.Value.top){
-                            cur_bound_box.Add(cur.Curve);
+                            // not yet used, need for later "mark" feature
+                            string cur_zone_name = one_of_zone.Key;
+
+                            // loop over all grids
+                            foreach (ElementId grid_id in grids) {
+
+                                Grid cur = doc.GetElement(grid_id) as Grid;
+                                string cur_name = cur.Name;
+                                if (cur_name == one_of_zone.Value.top) {
+                                    cur_bound_box.Add(cur.Curve);
+                                }
+                                if (cur_name == one_of_zone.Value.bottom) {
+                                    cur_bound_box.Add(cur.Curve);
+                                }
+                                if (cur_name == one_of_zone.Value.left) {
+                                    cur_bound_box.Add(cur.Curve);
+                                }
+                                if (cur_name == one_of_zone.Value.right) {
+                                    cur_bound_box.Add(cur.Curve);
+                                }
+
+                            }
+                            // now four bounding curve objects are added to cur_bound_box list
+                            // try to find XYZ for them:
+                            Curve top_ = cur_bound_box[0];
+                            Curve bottom_ = cur_bound_box[1];
+                            Curve left_ = cur_bound_box[2];
+                            Curve right_ = cur_bound_box[3];
+
+                            double topleft_x = left_.GetEndPoint(0).X;
+                            double topleft_y = top_.GetEndPoint(0).Y;
+
+                            XYZ topleft = new XYZ(topleft_x, topleft_y, slab_top_face_z);
+
+                            double topright_x = right_.GetEndPoint(0).X;
+                            double topright_y = top_.GetEndPoint(0).Y;
+                            XYZ topright = new XYZ(topright_x, topright_y, slab_top_face_z);
+
+                            double botleft_x = left_.GetEndPoint(0).X;
+                            double botleft_y = bottom_.GetEndPoint(0).Y;
+                            XYZ botleft = new XYZ(botleft_x, botleft_y, slab_top_face_z);
+
+                            double botright_x = right_.GetEndPoint(0).X;
+                            double botright_y = bottom_.GetEndPoint(0).Y;
+                            XYZ botright = new XYZ(botright_x, botright_y, slab_top_face_z);
+
+                            Curve top_trim = Line.CreateBound(topleft, topright);
+                            curve_trim.Add(top_trim);
+                            Curve bot_trim = Line.CreateBound(botleft, botright);
+                            curve_trim.Add(bot_trim);
+                            Curve left_trim = Line.CreateBound(topleft, botleft);
+                            curve_trim.Add(left_trim);
+                            Curve right_trim = Line.CreateBound(topright, botright);
+                            curve_trim.Add(right_trim);
                         }
-                        if (cur_name == one_of_zone.Value.bottom) {
-                            cur_bound_box.Add(cur.Curve);
+
+                        HostObject hostObj = doc.GetElement(s_id) as HostObject;
+                        Reference r = HostObjectUtils.GetTopFaces(hostObj).First();
+                        ICollection<ElementId> intersectingReferenceIds = new List<ElementId>();
+                        using (Transaction t = new Transaction(doc, "Divide Part at Grids")) {
+                            t.Start();
+                            //Transaction sketchPlaneTransaction = new Transaction(doc, "Create Sketch Plane");
+                            SketchPlane grid_sketchPlane = SketchPlane.Create(doc, r);
+                            //sketchPlaneTransaction.Commit();
+                            PartUtils.DivideParts(doc, partsList, intersectingReferenceIds, curve_trim, grid_sketchPlane.Id);
+                            t.Commit();
                         }
-                        if (cur_name == one_of_zone.Value.left) {
-                            cur_bound_box.Add(cur.Curve);
-                        }
-                        if (cur_name == one_of_zone.Value.right) {
-                            cur_bound_box.Add(cur.Curve);
-                        }
-                    
+
                     }
-                    // now four bounding curve objects are added to cur_bound_box list
-                    // try to find XYZ for them:
-                    Curve top_ = cur_bound_box[0];
-                    Curve bottom_ = cur_bound_box[1];
-                    Curve left_ = cur_bound_box[2];
-                    Curve right_ = cur_bound_box[3];
-
-                    double topleft_x = left_.GetEndPoint(0).X;
-                    double topleft_y = top_.GetEndPoint(0).Y;
-                    
-                    XYZ topleft = new XYZ(topleft_x, topleft_y, slab_top_face_z);
-
-                    double topright_x = right_.GetEndPoint(0).X;
-                    double topright_y = top_.GetEndPoint(0).Y;
-                    XYZ topright = new XYZ(topright_x, topright_y, slab_top_face_z);
-
-                    double botleft_x = left_.GetEndPoint(0).X;
-                    double botleft_y = bottom_.GetEndPoint(0).Y;
-                    XYZ botleft = new XYZ(botleft_x, botleft_y, slab_top_face_z);
-
-                    double botright_x = right_.GetEndPoint(0).X;
-                    double botright_y = bottom_.GetEndPoint(0).Y;
-                    XYZ botright = new XYZ(botright_x, botright_y, slab_top_face_z);
-
-                    Curve top_trim = Line.CreateBound(topleft, topright);
-                    curve_trim.Add(top_trim);
-                    Curve bot_trim = Line.CreateBound(botleft, botright);
-                    curve_trim.Add(bot_trim);
-                    Curve left_trim = Line.CreateBound(topleft, botleft);
-                    curve_trim.Add(left_trim);
-                    Curve right_trim = Line.CreateBound(topright, botright);
-                    curve_trim.Add(right_trim);
                 }
-
-                HostObject hostObj = doc.GetElement(s_id) as HostObject;
-                Reference r = HostObjectUtils.GetTopFaces(hostObj).First();
-                ICollection<ElementId> intersectingReferenceIds = new List<ElementId>();
-                using (Transaction t = new Transaction(doc, "Divide Part at Grids")) {
-                    t.Start();
-                    //Transaction sketchPlaneTransaction = new Transaction(doc, "Create Sketch Plane");
-                    SketchPlane grid_sketchPlane = SketchPlane.Create(doc, r);
-                    //sketchPlaneTransaction.Commit();
-                    PartUtils.DivideParts(doc, partsList, intersectingReferenceIds, curve_trim, grid_sketchPlane.Id);
-                    t.Commit();
-                }
-
             }
-    // Set the view's "Parts Visibility" parameter so that parts are shown
+            // Set the view's "Parts Visibility" parameter so that parts are shown
             Parameter p = doc.ActiveView.get_Parameter(BuiltInParameter.VIEW_PARTS_VISIBILITY);
-            using (Transaction t = new Transaction(doc, "Set View Parameter"))
-            {
+            using (Transaction t = new Transaction(doc, "Set View Parameter")) {
                 t.Start();
                 p.Set(0); // 0 = Show Parts, 1 = Show Original, 2 = Show Both
                 t.Commit();
@@ -230,3 +228,4 @@ namespace dividePart_test
         }
     }
 }
+ 
--- dividePart_test/dividePart_test/Program.cs
+++ dividePart_test/dividePart_test/Program.cs
@@ -8,48 +8,51 @@ using Newtonsoft.Json;
 
 namespace jsonDeserializer
 {
-    public class zoneAllocation
-    {
+    public class levelAllocation {
+        public Dictionary<string, zoneAllocation> Levels { set; get; }
+    }
+
+    public class zoneAllocation {
         public Dictionary<string, zone> Zones { set; get; }
     }
 
-    public class zone
-    {
-        public string top { get; set; }
+    public class zone {
         public string bottom { get; set; }
         public string left { get; set; }
         public string right { get; set; }
+        public string top { get; set; }
     }
+}
 
-    //class Program
-    //{
-    //    static void Main(string[] args)
-    //    {
+    //class Program {
+    //    static void Main(string[] args) {
     //        // import a file into code
     //        // change path to load different files
-    //        string path = @"D:\Documents\Revit Model\api_input_test.json";
+    //        string path = @"D:\Documents\Revit Model\parsed.json";
 
-    //        try
-    //        {   // Open the text file using a stream reader.
-    //            using (StreamReader file = File.OpenText(path))
-    //            {
+    //        try {   // Open the text file using a stream reader.
+    //            using (StreamReader file = File.OpenText(path)) {
     //                Console.WriteLine("inside file");
-    //                var res = JsonConvert.DeserializeObject<zoneAllocation>(File.ReadAllText(path));
+    //                var res = JsonConvert.DeserializeObject<levelAllocation>(File.ReadAllText(path));
+    //                //Dictionary<string, string> res = JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText(path));
     //                Console.WriteLine("res created");
-    //                foreach (var f in res.Zones)
-    //                {
-    //                    Console.WriteLine("inside forloop");
-    //                    Console.WriteLine("zone: {0}",f.Key);
-    //                    Console.WriteLine("top: {0}", f.Value.top);
-    //                    Console.WriteLine("bottom: {0}", f.Value.bottom);
-    //                    Console.WriteLine("left: {0}", f.Value.left);
-    //                    Console.WriteLine("right: {0}", f.Value.right);
+    //                foreach (var f in res.Levels) {
+    //                    //Console.WriteLine("inside forloop");
+    //                    Console.WriteLine("zone: {0}", f.Key);
+
     //                }
+    //                //foreach (var f in res.Levels) {
+    //                //    Console.WriteLine("inside forloop");
+    //                //    Console.WriteLine("zone: {0}", f.Key);
+    //                //    Console.WriteLine("top: {0}", f.Value.top);
+    //                //    Console.WriteLine("bottom: {0}", f.Value.bottom);
+    //                //    Console.WriteLine("left: {0}", f.Value.left);
+    //                //    Console.WriteLine("right: {0}", f.Value.right);
+    //                //}
 
     //            }
     //        }
-    //        catch (Exception e)
-    //        {
+    //        catch (Exception e) {
     //            Console.WriteLine("The file could not be read:");
     //            Console.WriteLine(e.Message);
     //        }
@@ -57,4 +60,3 @@ namespace jsonDeserializer
 
     //    }
     //}
-}
